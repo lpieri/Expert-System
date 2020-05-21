9a816ddb02bcf686955deda05a9c7651ba64ee0b
Otterqueen <mchapard@student.le-101.fr>
2020-05-21 02:41:49 +0200

    wip mieux mais decouverte pb negatio

--- src/solver.go
+++ src/solver.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -14,9 +15,8 @@ import (
 */
 
 func rulesLoop(querie string, rules []sRule) {
-	//for i := 0; i < len(rules); i++ {
-
-	for i := 0; i < 1; i++ {
+	for i := 0; i < len(rules); i++ {
+		fmt.Printf("Vars = %#v\n", vars)
 		fmt.Println(rules[i])
 		//* fonction resolve ONE rule */
 		//on remplace les variables
@@ -24,32 +24,55 @@ func rulesLoop(querie string, rules []sRule) {
 		fmt.Println("impliquent= ", rules[i].Conclusion)
 		fmt.Println("len faits= ", len(rules[i].Facts))
 		fmt.Println("len Conclusion= ", len(rules[i].Conclusion))
-		/*test si presence not*/
+
+		/*-----------------------------------  CAS N*1 : A = B               ---------------------------------------*/
 		if len(rules[i].Facts) == 1 && len(rules[i].Conclusion) == 1 {
 			negation := false
+			fmt.Println("rules[i].Facts[0] = ", rules[i].Facts[0])
 			if strings.Contains(rules[i].Facts[0], "!") || strings.Contains(rules[i].Conclusion[0], "!") {
 				negation = true
+				fmt.Println("NEGATION")
 			}
 			if vars[rules[i].Facts[0]] != "" && vars[rules[i].Conclusion[0]] != "" { // si existent deja toute les 2
+				fmt.Println("Toutes les 2 pas vide = ", vars[rules[i].Facts[0]], "     |      ", vars[rules[i].Conclusion[0]])
 				if vars[rules[i].Facts[0]] == vars[rules[i].Conclusion[0]] { //on verifie qu'elles sont egales
 					continue
 				} else { //sinon on print err contradictoire
 					printErrorMsg("System as contradictions please review input")
 				}
-			} else { // sinon on assigne la valur de celle qui n'est pas vide à celle qui l'est
+			} else { // sinon on assigne la valeur de celle qui n'est pas vide à celle qui l'est
+				fmt.Println("UNE des  2 vide = ", vars[rules[i].Facts[0]], "     |      ", vars[rules[i].Conclusion[0]])
+
 				if vars[rules[i].Facts[0]] != "" {
-					if negation && vars[rules[i].Facts[0]] == "true" {
-						vars[rules[i].Conclusion[0]] = "false"
-					} else if negation && vars[rules[i].Facts[0]] == "false" {
-						vars[rules[i].Conclusion[0]] = "true"
+					bFacts, err2 := strconv.ParseBool(vars[rules[i].Facts[0]])
+					if err2 == nil {
+						fmt.Println("fact pas vide =", vars[rules[i].Facts[0]])
+						if negation {
+							vars[rules[i].Conclusion[0]] = strconv.FormatBool(!bFacts)
+						} else {
+							vars[rules[i].Conclusion[0]] = vars[rules[i].Facts[0]]
+						}
 					} else {
-						vars[rules[i].Conclusion[0]] = vars[rules[i].Facts[0]]
+						printError(err2)
 					}
 				} else {
-					vars[rules[i].Facts[0]] = vars[rules[i].Conclusion[0]]
+					bConclusion, err := strconv.ParseBool(vars[rules[i].Conclusion[0]])
+					if err == nil {
+						fmt.Println("Conclusion pas vide =", vars[rules[i].Conclusion[0]])
+						if negation {
+							vars[rules[i].Facts[0]] = strconv.FormatBool(!bConclusion)
+						} else {
+							vars[rules[i].Facts[0]] = vars[rules[i].Conclusion[0]]
+						}
+					} else {
+						printError(err)
+					}
 				}
 			}
 		}
+		fmt.Printf("Vars = %#v\n", vars)
+		/*-----------------------------------  CAS N*1 : A + !B | C = D      ---------------------------------------*/
+		/*-----------------------------------  CAS N*1 : A + !B | C = D + F  ---------------------------------------*/
 		//on rajoute les valeurs trouvées et on return ok
 		//ou on ne trouve rien et on retrurn false
 	}
